be6ac1f158407bd315f1efa69c01552bdfeb03cb
FranciscoTurro <franciscogtrro@outlook.com.ar>
2022-07-19 19:57:22 -0300

    slow progress

--- script.js
+++ script.js
@@ -7,3 +7,8 @@ const display = (() => {})();
 const gameController = (() => {})();
 
 const players = (number) => {};
+
+const board = document.getElementById("board"); //know which cell i just clicked where does this go? display maybe
+board.addEventListener("click", (e) => {
+  if (e.target.dataset.cell != null) alert(e.target.dataset.cell);
+});

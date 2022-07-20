851c5e479ead8a6ecdf26fc124445689a5a98c5f
FranciscoTurro <franciscogtrro@outlook.com.ar>
2022-07-20 12:29:52 -0300

    not too shabby

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 const gameBoard = (() => {
   const Board = new Array(9);
+
+  const boardDOM = document.getElementById("board");
+
+  boardDOM.addEventListener("click", (e) => {
+    if (e.target.dataset.cell != null) {
+      Board[e.target.dataset.cell] = "x";
+    }
+  });
+  return { Board, boardDOM };
 })();
 
-const display = (() => {})();
+const display = (() => {
+  const draw = () => {
+    for (i = 0; i < gameBoard.Board.length; i++) {
+      if (gameBoard.Board[i] != null) {
+        gameBoard.boardDOM
+          .querySelector(`[data-cell="${i}"]`) //gives a different class to the square that has been selected, so i can go into css and make .classname to have a drawing or some shit
+          .classList.add("Asd");
+      }
+    }
+  };
+  return { draw };
+})();
 
 const gameController = (() => {})();
 
 const players = (number) => {};
 
-const board = document.getElementById("board"); //know which cell i just clicked where does this go? display maybe
-board.addEventListener("click", (e) => {
-  if (e.target.dataset.cell != null) alert(e.target.dataset.cell);
-});
+function asd() {
+  for (i = 0; i < gameBoard.Board.length; i++)
+    console.log(i + ": " + gameBoard.Board[i]);
+}

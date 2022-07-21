8abc60e69578aa97fd027529f367c98ecdd2a6d6
FranciscoTurro <franciscogtrro@outlook.com.ar>
2022-07-21 18:58:47 -0300

    half works. kinks to be ironed out still

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
+const BoardLength = 9;
+
 const gameBoard = (() => {
   const Board = new Array(9);
-
-  const boardDOM = document.getElementById("board");
 
   const getArrayInfo = (index) => {
     //i dont want users accessing my array
     return Board[index];
   };
 
+  const boardDOM = document.getElementById("board");
+
   boardDOM.addEventListener("click", (e) => {
     if (Board[e.target.dataset.cell] == null) {
       Board[e.target.dataset.cell] = gameController.getTurn();
       display.draw();
-      // gameController.checkForGame();
+      gameController.checkForGame();
       gameController.changeTurn();
     } else {
       return;
@@ -23,7 +25,7 @@ const gameBoard = (() => {
 
 const display = (() => {
   const draw = () => {
-    for (i = 0; i < 9; i++) {
+    for (i = 0; i < BoardLength; i++) {
       if (gameBoard.getArrayInfo(i) != null) {
         gameBoard.boardDOM
           .querySelector(`[data-cell="${i}"]`) //gives a different class to the square that has been selected, so i can go into css and make .classname to have a drawing or some shit
@@ -35,36 +37,60 @@ const display = (() => {
 })();
 
 const gameController = (() => {
+  //https://gist.github.com/perlmonger42/3543760
   const checkForGame = () => {
     if (
-      //checking if the array has a win condition. done in the single worst way possible. piss off posible solucion mejor https://gist.github.com/perlmonger42/3543760
-      (gameBoard.Board[0] == "x" &&
-        gameBoard.Board[1] == "x" &&
-        gameBoard.Board[2] == "x") ||
-      (gameBoard.Board[0] == "x" &&
-        gameBoard.Board[3] == "x" &&
-        gameBoard.Board[6] == "x") ||
-      (gameBoard.Board[2] == "x" &&
-        gameBoard.Board[5] == "x" &&
-        gameBoard.Board[8] == "x") ||
-      (gameBoard.Board[6] == "x" &&
-        gameBoard.Board[7] == "x" &&
-        gameBoard.Board[8] == "x") ||
-      (gameBoard.Board[0] == "x" &&
-        gameBoard.Board[4] == "x" &&
-        gameBoard.Board[8] == "x") ||
-      (gameBoard.Board[2] == "x" &&
-        gameBoard.Board[4] == "x" &&
-        gameBoard.Board[6] == "x") ||
-      (gameBoard.Board[1] == "x" &&
-        gameBoard.Board[4] == "x" &&
-        gameBoard.Board[7] == "x") ||
-      (gameBoard.Board[3] == "x" &&
-        gameBoard.Board[4] == "x" &&
-        gameBoard.Board[5] == "x")
+      winCondition(
+        gameBoard.getArrayInfo(0),
+        gameBoard.getArrayInfo(1),
+        gameBoard.getArrayInfo(2)
+      ) ||
+      winCondition(
+        gameBoard.getArrayInfo(0),
+        gameBoard.getArrayInfo(3),
+        gameBoard.getArrayInfo(6)
+      ) ||
+      winCondition(
+        gameBoard.getArrayInfo(2),
+        gameBoard.getArrayInfo(5),
+        gameBoard.getArrayInfo(8)
+      ) ||
+      winCondition(
+        gameBoard.getArrayInfo(6),
+        gameBoard.getArrayInfo(7),
+        gameBoard.getArrayInfo(8)
+      ) ||
+      winCondition(
+        gameBoard.getArrayInfo(0),
+        gameBoard.getArrayInfo(4),
+        gameBoard.getArrayInfo(8)
+      ) ||
+      winCondition(
+        gameBoard.getArrayInfo(2),
+        gameBoard.getArrayInfo(4),
+        gameBoard.getArrayInfo(6)
+      ) ||
+      winCondition(
+        gameBoard.getArrayInfo(1),
+        gameBoard.getArrayInfo(4),
+        gameBoard.getArrayInfo(7)
+      ) ||
+      winCondition(
+        gameBoard.getArrayInfo(3),
+        gameBoard.getArrayInfo(4),
+        gameBoard.getArrayInfo(5)
+      )
     ) {
-      alert("you won now fuck off");
+      alert(`The player with ${getTurn()} wins`);
       location.reload(); //TEMPORARY
+    }
+  };
+
+  const winCondition = (n1, n2, n3) => {
+    if (n1 != null) {
+      if (n1 == n2 && n1 == n3) {
+        return true;
+      }
     }
   };
 
@@ -82,14 +108,15 @@ const gameController = (() => {
     return gameController.currentTurn;
   };
 
-  return { checkForGame, getTurn, changeTurn };
+  return { checkForGame, getTurn, changeTurn }; //try not to allow users to change the turn
 })();
 
 const players = (number) => {};
 
 function asd() {
   //troubleshooting, ignore
-  for (i = 0; i < 9; i++) console.log(i + ": " + gameBoard.getArrayInfo(i));
+  for (i = 0; i < BoardLength; i++)
+    console.log(i + ": " + gameBoard.getArrayInfo(i));
 }
 
 window.onload = gameController.changeTurn(); //must be here, signs OF GOOD CODING AM I RIGHT FELLAS
